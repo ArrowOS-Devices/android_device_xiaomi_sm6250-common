f8e5784437c4600513bf7d70c802fe9b0586e8b6
SiddharthBharadwaj <siddharthbharadwaj45@gmail.com>
2020-06-20 20:31:13 +0000

    sm6250-common: XiaomiParts: Add SELinux toggle
    Reference: DerpLab/platform_packages_apps_DerpQuest@65f48a6
    
    Vectors From: https://materialdesignicons.com
    
    @SiddharthBharadwaj edits: Adapt To Parts
    
    Following are neverallows below, no idea how to fix it yet
    
    allow init su:process { noatsecure }
    allow su vendor_file:file { entrypoint };
    allow su shell_exec:file { execute  getattr  read };
    
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- XiaomiParts/src/com/xiaomi/parts/BootReceiver.java
+++ XiaomiParts/src/com/xiaomi/parts/BootReceiver.java
@@ -16,19 +16,87 @@
 
 package com.xiaomi.parts;
 
+import android.app.ActivityManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import androidx.preference.PreferenceManager;
 import android.provider.Settings;
+import android.content.SharedPreferences;
+import android.os.SELinux;
+import android.util.Log;
+import android.widget.Toast;
+import com.xiaomi.parts.R;
 
 import com.xiaomi.parts.kcal.Utils;
 import com.xiaomi.parts.ambient.SensorsDozeService;
 
+import java.io.IOException;
+import java.util.List;
+
 public class BootReceiver extends BroadcastReceiver implements Utils {
 
+    private static final String PREF_SELINUX_MODE = "selinux_mode";
+
+    private Context settingsContext = null;
+    private static final String TAG = "SettingsOnBoot";
+    private boolean mSetupRunning = false;
+    private Context mContext;
+
     public void onReceive(Context context, Intent intent) {
+
+    mContext = context;
+    ActivityManager activityManager =
+            (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+    List<ActivityManager.RunningAppProcessInfo> procInfos =
+            activityManager.getRunningAppProcesses();
+    for(int i = 0; i < procInfos.size(); i++) {
+        if(procInfos.get(i).processName.equals("com.google.android.setupwizard")) {
+            mSetupRunning = true;
+        }
+    }
+
+    if(!mSetupRunning) {
+        try {
+            settingsContext = context.createPackageContext("com.android.settings", 0);
+        } catch (Exception e) {
+            Log.e(TAG, "Package not found", e);
+        }
+        SharedPreferences sharedpreferences = context.getSharedPreferences("selinux_pref",
+                Context.MODE_PRIVATE);
+        if (sharedpreferences.contains(PREF_SELINUX_MODE)) {
+        boolean currentIsSelinuxEnforcing = SELinux.isSELinuxEnforced();
+        boolean isSelinuxEnforcing =
+                    sharedpreferences.getBoolean(PREF_SELINUX_MODE,
+                            currentIsSelinuxEnforcing);
+            if (isSelinuxEnforcing) {
+               if (!currentIsSelinuxEnforcing) {
+                   try {
+                        SuShell.runWithSuCheck("setenforce 1");
+                        showToast(context.getString(R.string.selinux_enforcing_toast_title),
+                                context);
+                    } catch (SuShell.SuDeniedException e) {
+                        showToast(context.getString(R.string.cannot_get_su), context);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+               }
+            } else {
+                if (currentIsSelinuxEnforcing) {
+                    try {
+                        SuShell.runWithSuCheck("setenforce 0");
+                        showToast(context.getString(R.string.selinux_permissive_toast_title),
+                                context);
+                    } catch (SuShell.SuDeniedException e) {
+                        showToast(context.getString(R.string.cannot_get_su), context);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
 
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
 
@@ -72,5 +140,10 @@ public class BootReceiver extends BroadcastReceiver implements Utils {
         if (enabled) {
             context.startService(new Intent(context, FPSInfoService.class));
         }
+    }
+
+    private void showToast(String toastString, Context context) {
+    Toast.makeText(context, toastString, Toast.LENGTH_SHORT)
+            .show();
     }
 }
