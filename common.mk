b51c332d3d8717eefd7f421969dd645a00a816c3
rcstar6696 <rcstar6696@gmail.com>
2020-07-03 13:39:30 +0200

    sm6250-common: Add XiaomiDoze
    Signed-off-by: sreekfreak995 <sreekfreak995@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- common.mk
+++ common.mk
@@ -146,6 +146,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.3-service.clearkey
 
+# Doze
+PRODUCT_PACKAGES += \
+    XiaomiDoze
+
 # Fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
