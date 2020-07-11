3c5c2490c7907674d2dce53e27442a272c4b5c10
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2020-07-11 23:58:54 +0530

    sm6250-common: power-libperfmgr: adapt and rebrand for xiaomi_sm6250
    Change-Id: Ia9643f9b842849b6e9cf5a987fc84fc02dcb9c7e

--- power-libperfmgr/display-helper.cpp
+++ power-libperfmgr/display-helper.cpp
@@ -15,7 +15,7 @@
  */
 
 #define LOG_NIDEBUG 0
-#define LOG_TAG "android.hardware.power@1.3-service.pixel-libperfmgr"
+#define LOG_TAG "android.hardware.power@1.3-service.xiaomi_sm6250-libperfmgr"
 
 #include <dlfcn.h>
 #include <errno.h>
