f7004058d44876a432d416375756c031b40b99b4
Bharat Pawar <bpawar@codeaurora.org>
2020-07-30 20:18:30 +0530

    sm6250-common: vibrator: Enable LED based vibrator
    Enable LED based vibrator for talos, moorea and
    atoll targets.
    
    Change-Id: I079eefaefff77fe19304acc8749773febf91a7fc

--- common.mk
+++ common.mk
@@ -280,6 +280,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator.service
+
 # VNDK
 # FIXME: master: compat for libprotobuf
 # See https://android-review.googlesource.com/c/platform/prebuilts/vndk/v28/+/1109518
