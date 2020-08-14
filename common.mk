099ca91c6165a1eea7b8527f83702642ce770c44
Sonal Singh <sonal.singh.19993@gmail.com>
2020-08-14 19:51:15 +0530

    sm6250-common: lineage -> arrow
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- common.mk
+++ common.mk
@@ -175,13 +175,12 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage \
+    $(LOCAL_PATH)/overlay-arrow \
     $(LOCAL_PATH)/overlay-system
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
-    $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap \
+    $(LOCAL_PATH)/overlay-xtended/packages/apps/Snap \
     $(LOCAL_PATH)/overlay-system
 
 # Partitions
