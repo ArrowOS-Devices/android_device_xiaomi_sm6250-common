616677d619ed7a4315e6b2e458d0912fa65b8260
Nick C <ctwoon@suicide.today>
2020-08-25 11:39:08 +0300

    sm6250-common: add FM

--- common.mk
+++ common.mk
@@ -30,6 +30,16 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml
 
+# FM
+PRODUCT_PACKAGES += \
+    FM2 \
+    libqcomfm_jni \
+    qcom.fmradio \
+    qcom.fmradio.xml
+
+PRODUCT_BOOT_JARS += \
+    qcom.fmradio
+
 # AVB
 PRODUCT_PACKAGES += \
     q-gsi.avbpubkey \
