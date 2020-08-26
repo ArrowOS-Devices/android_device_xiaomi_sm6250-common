f7c0a9c5546970ce83ca8ac1bd24de57720a2c37
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2020-08-26 11:10:57 +0530

    sm6250-common: Switch to sunfish cpuset setup
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@gmail.com>

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -234,10 +234,11 @@ function configure_memory_parameters() {
     done
 
     # cpuset parameters
-    echo 0-2         > /dev/cpuset/background/cpus
-    echo 0-3         > /dev/cpuset/system-background/cpus
-    echo 0-2,4-7 > /dev/cpuset/foreground/cpus
     echo 0-7         > /dev/cpuset/top-app/cpus
+    echo 0-5,7       > /dev/cpuset/foreground/cpus
+    echo 4-5         > /dev/cpuset/background/cpus
+    echo 2-5         > /dev/cpuset/system-background/cpus
+    echo 2-5         > /dev/cpuset/restricted/cpus
 
     # Turn on sleep modes
     echo 0 > /sys/module/lpm_levels/parameters/sleep_disabled
