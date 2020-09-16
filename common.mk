0a885b24b4befcb4e0085cd5a357b61011160222
Volodymyr Zhdanov <wight554@gmail.com>
2020-09-16 11:35:20 +0300

    sm6250-common: build libhidltransport and libhwbinder
    * also their vendor variants
    * for q blobs compatibility

--- common.mk
+++ common.mk
@@ -118,6 +118,12 @@ PRODUCT_PACKAGES += \
     android.hidl.manager@1.0 \
     android.hidl.manager@1.0_system
 
+PRODUCT_PACKAGES += \
+    libhidltransport \
+    libhidltransport.vendor \
+    libhwbinder \
+    libhwbinder.vendor
+
 # Hotword Enrollement
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/hotword-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/hotword-hiddenapi-package-whitelist.xml \
