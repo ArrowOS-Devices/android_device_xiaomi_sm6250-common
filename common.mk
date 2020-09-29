5aa10f208c044995cafee004d19e0d04bf9436ad
Volodymyr Zhdanov <wight554@gmail.com>
2020-09-29 21:47:10 +0300

    sm6250-common: add bt_stack_qti.conf to soong namespaces

--- common.mk
+++ common.mk
@@ -223,7 +223,8 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/pixel \
     hardware/xiaomi \
-    vendor/qcom/opensource/commonsys/packages/apps/Bluetooth
+    vendor/qcom/opensource/commonsys/packages/apps/Bluetooth \
+    vendor/qcom/opensource/commonsys/system/bt/conf
 
 # Telephony
 PRODUCT_PACKAGES += \
