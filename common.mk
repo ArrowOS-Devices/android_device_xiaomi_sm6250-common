0b149a2e80adb45490cf11ce5daf3927f4db5e60
Volodymyr Zhdanov <wight554@gmail.com>
2020-10-06 10:22:40 +0300

    sm6250-common: disable configstore
    * it's finally deprecated in R
    * ship disable_configstore because we have API level 29

--- common.mk
+++ common.mk
@@ -89,6 +89,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# Display
+PRODUCT_PACKAGES += \
+    disable_configstore
+
 # Doze
 PRODUCT_PACKAGES += \
     XiaomiDoze
