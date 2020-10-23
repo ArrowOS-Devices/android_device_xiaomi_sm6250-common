fd2c840644e3952a6030a8aa5f0d5d5a91b877e7
Volodymyr Zhdanov <wight554@gmail.com>
2020-10-23 10:11:55 +0300

    sm6250-common: build libqti_vndfwk_detect from source

--- common.mk
+++ common.mk
@@ -172,6 +172,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
 
+# Framework detect
+PRODUCT_PACKAGES += \
+    libqti_vndfwk_detect.vendor
+
 # Fstab
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom \
