bcc8b37bb433316a4a9da775329316ce232a917f
Rahul Shahare <rshaha@codeaurora.org>
2020-12-07 21:31:10 +0530

    sm6250-common: Add kpti=off to boot command line
    This will OFF kernel page table isolation.
    
    Notes from Kuba Wojciechowski <nullbytepl@gmail.com> in https://github.com/tadiphone-caf/msm-4.14/commit/86bb20a8325d32ac678573c86874f36ec4575ee2:
    As per qualcomm - "SM8150/SM8250/SM8350/SM7250/SM7150/SM6150 - KPTI Not required".
    It can also help increase performance by a lot in some scenarios.
    
    Change-Id: Idd36c359d4955701e9ee3ee27a58177a8d622c86
    Signed-off-by: Rahul Shahare <rshaha@codeaurora.org>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -108,6 +108,7 @@ BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=1
 BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
+BOARD_KERNEL_CMDLINE += kpti=off
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
