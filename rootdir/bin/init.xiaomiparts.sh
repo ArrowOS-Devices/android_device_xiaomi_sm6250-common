238f0397015a559f48091daf8317546f63558d4b
Sonal Singh <sonal.singh.19993@gmail.com>
2021-04-07 13:10:46 +0530

    [SQUASH] sm6250-common: XiaomiParts: Get rid of some unneeded features
    Revert "sm6250-common: XiaomiParts: Add GPU Boost"
    
    This reverts commit 2bf707d56b724d70ddab7e50e7be3d327ff03702.
    
    Revert "[SQUASH] sm6250-common: XiaomiParts: Introduce smart charging"
    
    This reverts commit 84e8cd84ba6688fb3b507354364e51c881a9c5db.
    
    Revert "sm6250-common: XiaomiParts: USB Fastcharge improvement"
    
    This reverts commit dcaa258d8f5f6cc21787a20b50961274def9fd9b.
    
    Revert "sm6250-common: XiaomiParts: Touchboost Improvements"
    
    This reverts commit cfecc60c9fa048834cc9e5c2d3f9cf75a32ab0a8.
    
    Revert "sm6250-common: XiaomiParts: Add Touchboost toggle"
    
    This reverts commit 4de96bd421dd94b0bd8a56fe076f44fe23eb6073.

--- rootdir/bin/init.xiaomiparts.sh
+++ rootdir/bin/init.xiaomiparts.sh
@@ -1,6 +1,3 @@
 #!/system/bin/sh
 
-# If there is not a persist value, we need to set one
-if [[ `getprop persist.xiaomiparts.gpu_profile` == "" ]]; then
-    setprop persist.xiaomiparts.gpu_profile 0
-fi
+# Dummy executable, will be used later
