297c895f9bb2e74111a9c87a6184bc3942c33e4b
SonalSingh18 <sonal.singh.19993@gmail.com>
2021-04-07 19:00:00 +0100

    sm6250-common: lineage -> arrow
    Signed-off-by: SonalSingh18 <sonal.singh.19993@gmail.com>

--- common.mk
+++ common.mk
@@ -284,7 +284,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-arrow
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
