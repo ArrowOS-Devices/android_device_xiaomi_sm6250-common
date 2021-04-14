762f34ff6791d4b9009aeb217baf3387b927f340
LuK1337 <priv.luk@gmail.com>
2021-04-14 14:29:58 +0200

    sm6250-common: Import QTI vibrator HAL
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- common.mk
+++ common.mk
@@ -397,7 +397,7 @@ PRODUCT_COPY_FILES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
+    vendor.qti.hardware.vibrator.service.xiaomi_sm6250
 
 # VNDK
 PRODUCT_COPY_FILES += \
