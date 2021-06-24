6af2874ca6676fec78d02daff21b44ca7d19bddf
Alexander Winkowski <dereference23@outlook.com>
2021-06-24 10:32:21 +0200

    sm6250-common: Import excluded-input-devices.xml
    * From curtana-user 11 RKQ1.200826.002 V12.0.5.0.RJWINXM release-keys
    
    Change-Id: Iacecd13232e064e95e6bb3470763d12d10f4a0a3

--- common.mk
+++ common.mk
@@ -389,6 +389,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+
 # VNDK
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libhidlcache.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlcache.so
