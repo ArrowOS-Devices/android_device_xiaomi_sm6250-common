107f932441379ef84f8658d98e3849dee775a6ad
sairam1411 <majgaonkars3@gmail.com>
2021-07-06 17:42:27 +0530

    sm6250-common: Drop QCOM WFD blobs
    * Lets use aosp wfd which actually works
    
    Signed-off-by: sairam1411 <majgaonkars3@gmail.com>

--- common.mk
+++ common.mk
@@ -440,11 +440,7 @@ PRODUCT_COPY_FILES += \
 
 # WiFi Display
 PRODUCT_PACKAGES += \
-    libnl \
-    libwfdaac_vendor
-
-PRODUCT_BOOT_JARS += \
-    WfdCommon
+    libnl
 
 # XiaomiParts
 PRODUCT_PACKAGES += \
