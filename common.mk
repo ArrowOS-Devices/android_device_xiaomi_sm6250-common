6bc6ec748db254257c8b5d44ee5b96de01e8359a
Alexander Winkowski <dereference23@outlook.com>
2021-09-22 17:31:50 +0200

    sm6250-common: Update sensors service to 2.1
    Change-Id: Ie417407c36929cf34ecbdd28feb1cab996fe1f30

--- common.mk
+++ common.mk
@@ -317,7 +317,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service.multihal \
+    android.hardware.sensors@2.1-service.multihal \
     vendor.qti.hardware.display.mapper@1.1.vendor
 
 PRODUCT_COPY_FILES += \
