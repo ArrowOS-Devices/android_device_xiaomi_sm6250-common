a68e19032788460d853d526bd48425720f50d4fb
Sonal Singh <sonal.singh.19993@gmail.com>
2021-10-31 09:12:10 +0000

    sm6250-common: Restore QTI Bluetooth
    Revert "sm6250-common: Enable AOSP bluetooth audio HAL v2"
    
    This reverts commit d3eb7160aa5f07fd7f7d58ae0d4993475f3938e3.
    
    Revert "sm6250-common: Load A2DP configuration from a2dp_audio_policy_configuration.xml"
    
    This reverts commit c00fedcab02d42d7b6477def8dec1b1b270522fd.
    
    Revert "sm6250-common: Switch to AOSP BT"
    
    This reverts commit 143bfcb8820b321f8bea46da2b39c850d627b2fc.

--- common.mk
+++ common.mk
@@ -69,7 +69,8 @@ PRODUCT_COPY_FILES += \
 
 # Component override
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
+    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml \
+    $(LOCAL_PATH)/configs/component-overrides_qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/component-overrides.xml
 
 # FM
 PRODUCT_PACKAGES += \
@@ -86,7 +87,8 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    audio.bluetooth.default
+    audio.bluetooth.default \
+    libbthost_if
 
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
@@ -94,10 +96,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
-
-PRODUCT_COPY_FILES += \
-    frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
