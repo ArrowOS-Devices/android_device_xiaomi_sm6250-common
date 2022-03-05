355e9bb30ad142996658565a1cec1b19603d8373
Pranav Vashi <neobuddy89@gmail.com>
2022-03-05 22:52:13 +0530

    sm6250-common: releasetools: Use appropriate message on flashing vbmeta
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- releasetools.py
+++ releasetools.py
@@ -30,7 +30,7 @@ def AddImage(info, input_zip, basename, dest):
   info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
 
 def OTA_InstallEnd(info, input_zip):
-  info.script.Print("Patching firmware images...")
+  info.script.Print("Patching dtbo and vbmeta images...")
   AddImage(info, input_zip, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
   AddImage(info, input_zip, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
   AddImage(info, input_zip, "vbmeta_system.img", "/dev/block/bootdevice/by-name/vbmeta_system")
