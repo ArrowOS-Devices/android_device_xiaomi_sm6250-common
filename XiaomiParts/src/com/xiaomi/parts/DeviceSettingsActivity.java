3e234f7c7a7561cf5b176b88722bb513430f85e5
TheScarastic <warabhishek@gmail.com>
2022-03-08 08:07:28 +0000

    sm6250-common: XiaomiParts: Adapt to S style
    * Use Theme.SubSettingsBase for theme
    
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- XiaomiParts/src/com/xiaomi/parts/DeviceSettingsActivity.java
+++ XiaomiParts/src/com/xiaomi/parts/DeviceSettingsActivity.java
@@ -16,38 +16,20 @@
 
 package com.xiaomi.parts;
 
-import android.app.Activity;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.MenuItem;
 
-public class DeviceSettingsActivity extends Activity {
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import com.android.settingslib.collapsingtoolbar.R;
 
-    private DeviceSettings mDeviceSettingsFragment;
+public class DeviceSettingsActivity extends CollapsingToolbarBaseActivity {
+
+    private static final String TAG_DEVICESETTINGS = "devicesettings";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getActionBar().setDisplayHomeAsUpEnabled(true);
-
-        Fragment fragment = getFragmentManager().findFragmentById(android.R.id.content);
-        if (fragment == null) {
-            mDeviceSettingsFragment = new DeviceSettings();
-            getFragmentManager().beginTransaction()
-                    .add(android.R.id.content, mDeviceSettingsFragment)
-                    .commit();
-        } else {
-            mDeviceSettingsFragment = (DeviceSettings) fragment;
-        }
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            finish();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        getFragmentManager().beginTransaction().replace(R.id.content_frame,
+                new DeviceSettings(), TAG_DEVICESETTINGS).commit();
     }
 }
