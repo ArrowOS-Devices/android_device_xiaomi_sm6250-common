4336f613e6397e6796ddea70891e4dc50aed16c9
luk1337 <priv.luk@gmail.com>
2022-03-17 01:42:56 +0000

    [BLOBS] sm6250-common: Resurrect QCOM WFD for A12 [1/2]
    * use system stack from LA.QSSI.12.0.r1-05600-qssi.0
    * use vendor stack from LA.UM.9.1.r1-11500.02-SMxxx0.QSSI12.0
    
     - apply this as well e9ee01a9ba3aecba4813937687407ca9093a3373.
                          630d7bd0e639b03bf3efa564f0bf0110c0eca459.
    
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- common.mk
+++ common.mk
@@ -518,6 +518,17 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml \
     frameworks/native/data/etc/android.hardware.wifi.rtt.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.rtt.xml
 
+# WiFi Display
+PRODUCT_PACKAGES += \
+    libavservices_minijail_32 \
+    libdisplayconfig.system.qti \
+    libnl \
+    libqdMetaData.system \
+    libwfdaac_vendor
+
+PRODUCT_BOOT_JARS += \
+    WfdCommon
+
 # XiaomiDoze
 PRODUCT_PACKAGES += \
     XiaomiDoze
