0c00bc1158968be226593bf51021f02b0557cc24
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-03-23 14:05:02 +0600

    sm6250-common: properties: Enable bluetooth HAL to read mac address from NV
     * without this, it sets a random mac address

--- common.mk
+++ common.mk
@@ -104,6 +104,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.vendor.bluetooth.modem_nv_support=true
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
