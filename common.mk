ab954ef834711618b61a1da0433c9a119099a1ef
Sonal Singh <sonal.singh.19993@gmail.com>
2022-07-15 17:11:19 +0000

    [SQUASH] sm6250-common: Drop Alipay fingerprint payment support
    Partial Revert "sm6250-common: sepolicy: Attach vendor_property_type to properties"
    
    This partially reverts commit d6c00123048bf09d3c1e08e21d245e58b3383a0b.
    
    Partial Revert "sm6250-common: sepolicy: Adapt to Android 12"
    
    This partially reverts commit 70a7954578edf0d23c943db145a4cb8755180601.
    
    Revert "sm6250-common: sepolicy: Remove lineage context from mlipay attribute"
    
    This reverts commit 0cc28790ae213d8947057355a780968c5fb094df.
    
    Revert "sm6250-common: Commonize IFAAManager"
    
    This reverts commit ca81220534dae554fe3a91318537068bed3a5a46.
    
    Revert "sm6250-common: Make IFAAManager compile again"
    
    This reverts commit a1b3ce16b5668d7dfe27aa9556eec9cf6c1fd5ae.
    
    Revert "sm6250-common: Support Alipay fingerprint payment"
    
    This reverts commit c539086765bac134cc75d0ba7c97bf67c80c868a.
    
    Revert "[BLOBS] sm6250-common: import Mlipay blobs from joyeuse"
    
    This reverts commit 00fffcc0ac4d11c71839ac3b04a869ebd77aa3ad.

--- common.mk
+++ common.mk
@@ -224,13 +224,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
-# IFAA manager
-PRODUCT_PACKAGES += \
-    org.ifaa.android.manager
-
-PRODUCT_BOOT_JARS += \
-    org.ifaa.android.manager
-
 # Init scripts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/init.qcom.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.qcom.rc \
